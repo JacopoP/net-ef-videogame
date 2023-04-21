27b634b66ecd8e52a1947c0f86092fb7d46afb18
Jac22 <jacopo.prudentino@gmail.com>
2023-04-21 16:15:28 +0200

    Creata funzione per aggiunta di software house

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@
                 Console.WriteLine("Digita 2 per cercare un videogioco tramite id");
                 Console.WriteLine("Digita 3 per ricercare un videogioco in base al nome");
                 Console.WriteLine("Digita 4 per cancellare un videogioco");
+                Console.WriteLine("Digita 5 per inserire una nuova software house");
                 Console.WriteLine("Digita qualcos'altro per chiudere il programma\n");
                 if (!Int32.TryParse(Console.ReadLine(), out controller))
                     controller = 0;
@@ -41,7 +42,7 @@
                             } while (!DateTime.TryParse(Console.ReadLine(), out dataUscita));
                             do
                             {
-                                Console.WriteLine("Qual è la software house che l'ha rilascaito?");
+                                Console.WriteLine("Qual è l'id della software house che l'ha rilascaito?");
                             } while (!long.TryParse(Console.ReadLine(), out softwareHouse));
                             try
                             {
@@ -114,6 +115,43 @@
                                     db.SaveChanges();
                                     Console.WriteLine("Cancellato con successo!");
                                 }
+                            }
+                            catch (Exception ex) { Console.WriteLine(ex.Message); }
+                        }
+                        break;
+                    case 5:
+                        {
+                            string? nome;
+                            string? taxId;
+                            string? citta;
+                            string? stato;
+
+                            do
+                            {
+                                Console.WriteLine("Come si chiama la software house?");
+                                nome = Console.ReadLine();
+                            } while (nome == null);
+                            do
+                            {
+                                Console.WriteLine("Qual è la sua partita iva?");
+                                taxId = Console.ReadLine();
+                            } while (taxId == null);
+                            do
+                            {
+                                Console.WriteLine("In che città si trova?");
+                                citta = Console.ReadLine();
+                            } while (citta == null);
+                            do
+                            {
+                                Console.WriteLine("In che Stato si trova?");
+                                stato = Console.ReadLine();
+                            } while (stato == null);
+                            try
+                            {
+                                Software_house s = new Software_house(nome, taxId, citta, stato);
+                                db.Add(s);
+                                db.SaveChanges();
+                                Console.WriteLine(s.Name + " salvata con successo!");
                             }
                             catch (Exception ex) { Console.WriteLine(ex.Message); }
                         }
